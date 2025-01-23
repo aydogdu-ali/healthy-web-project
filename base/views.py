e6417d1e55dcad6a4153841adad8b73a262e0c4b
Ali <ali.ozgur@example.com>
2025-01-23 20:45:12 +0300

    load index.html

--- base/views.py
+++ base/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, "base/index.html")
+
+
+def service_detail(request, servise_id):
+    return render(request, "base/service_detail.html")
