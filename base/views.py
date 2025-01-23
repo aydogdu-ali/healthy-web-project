10628d59be2e7c278b156ec5b15bc1832a342b8d
Ali <ali.ozgur@example.com>
2025-01-23 23:17:46 +0300

    did service_detail page

--- base/views.py
+++ base/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 
+from base import models as base_models
+
 # Create your views here.
 def index(request):
-    return render(request, "base/index.html")
+    services = base_models.Service.objects.all()
+
+    context =  {
+        "services": services
+    }
+    return render(request, "base/index.html", context)
 
 
-def service_detail(request, servise_id):
-    return render(request, "base/service_detail.html")
+def service_detail(request, service_id):
+    service = base_models.Service.objects.get(id=service_id)
+
+    context =  {
+        "service": service
+    }
+    return render(request, "base/service_detail.html", {"service":service})
