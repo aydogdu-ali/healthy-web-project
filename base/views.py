9546f630bb3ce52d7083805c479c1a67816324ad
Ali <ali.ozgur@example.com>
2025-01-24 20:43:33 +0300

    add env files

--- base/views.py
+++ base/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 
 from django.contrib.auth.decorators import login_required
+from django.conf import settings
+
 from base import models as base_models
 from doctor import models as doctor_models
 from patient import models as patient_models
@@ -89,6 +91,9 @@ def checkout(request,billing_id):
     
     context = {
 
-         "billing":billing
+         "billing":billing,
+         "stripe_public_key":settings.STRIPE_PUBLIC_KEY,
+         "paypal_client_id":settings.PAYPAL_CLIENT_ID,
+
      }
     return render(request, "base/checkout.html", context)
