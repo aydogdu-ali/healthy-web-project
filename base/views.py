7bc6a7017d047ac24cc1cfaa17317c10faccd1cf
Ali <ali.ozgur@example.com>
2025-01-26 13:51:45 +0300

    add payment-status page and viwes

--- base/views.py
+++ base/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
 from django.urls import reverse
+from django.http import JsonResponse
+
 
 import stripe
 from base import models as base_models
@@ -76,7 +78,7 @@ def book_appointment(request, service_id, doctor_id):
         billing.sub_total=appoinment.service.cost
         billing.tax=appoinment.service.cost*18/100
         billing.total = billing.sub_total+billing.tax
-        billing.status="unpaid"
+        billing.status="Unpaid"
         billing.save()
 
         return redirect("base:checkout",billing.billing_id)
@@ -96,7 +98,7 @@ def checkout(request,billing_id):
     context = {
 
          "billing":billing,
-         "stripe_public_key":settings.STRIPE_PUBLIC_KEY,
+         "stripe_public_key": settings.STRIPE_PUBLIC_KEY,
          "paypal_client_id":settings.PAYPAL_CLIENT_ID,
 
      }
@@ -105,5 +107,71 @@ def checkout(request,billing_id):
 
 @csrf_exempt
 def stripe_payment(request, billing_id):
-    billing= base_models.Billing.objects.get(billing_id)
+    billing= base_models.Billing.objects.get(billing_id=billing_id)
     stripe.api_key = settings.STRIPE_SECRET_KEY
+
+    checkout_session =stripe.checkout.Session.create(
+        customer_email=billing.patient.email,
+        payment_method_types= ['card'],
+         line_items = [
+            {
+                'price_data': {
+                    'currency': 'USD',
+                    'product_data': {
+                        'name': billing.patient.full_name
+                    },
+                    'unit_amount': int(billing.total * 100)
+                },
+                'quantity': 1
+            }
+        ],
+        mode = 'payment',
+        success_url = request.build_absolute_uri(reverse("base:stripe_payment_verify", args=[billing.billing_id])) + "?session_id={CHECKOUT_SESSION_ID}",
+        cancel_url=request.build_absolute_uri(reverse("base:stripe_payment_verify", args=[billing.billing_id])) + "?session_id={CHECKOUT_SESSION_ID}"
+    )
+
+    return JsonResponse({"sessionId": checkout_session.id})
+        
+def stripe_payment_verify(request, billing_id):
+    billing= base_models.Billing.objects.get(billing_id=billing_id)
+    session_id= request.GET.get("session_id")
+    session = stripe.checkout.Session.retrieve(session_id)
+
+    if session.payment_status =="paid":
+        if billing.status =="Unpaid":
+            billing.status= "Paid"
+            billing.save()
+            billing.appointment.status =="Completed"
+            billing.appointment.save()
+
+            doctor_models.Notification.objects.create(
+                doctor= billing.appointment.doctor,
+                appointment=billing.appointment,
+                type= "New Appointment"
+
+            )
+
+
+            patient_models.Notification.objects.create(
+                patient= billing.appointment.patient,
+                appointment=billing.appointment,
+                type= "Appointment Scheduled"
+
+            )
+    
+            return redirect( f"/payment_status/{billing.billing_id}/?payment_status=paid")
+    else:
+        return redirect( f"/payment_status/{billing.billing_id}/?payment_status=failed")
+    
+@login_required
+def payment_status(request, billing_id):
+    billing= base_models.Billing.objects.get(billing_id=billing_id)
+    payment_status= request.GET.get("payment_status")
+
+    context = {
+
+        "billing":billing,
+        "payment_status":payment_status,
+    }
+
+    return render(request, "base/payment_status.html", context)
