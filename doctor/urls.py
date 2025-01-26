940a733021c565848b4eab98fa4074fb5fbb39f7
Ali <ali.ozgur@example.com>
2025-01-26 22:53:29 +0300

    added appointment pages and dashboard pages

---
+++ doctor/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from doctor import views 
+
+app_name="doctor"
+
+urlpatterns = [
+    path("", views.dashboard, name="dashboard"),
+    path("appointments/", views.appointments, name="appointments"),
+
+     path("appointments/<appointment_id>/", views.appointment_detail, name="appointment_detail"),
+
+]
--- doctor/views.py
+++ doctor/views.py
@@ -1,3 +1,58 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+from base import models as base_models
+from doctor import models as doctor_models
+
+
+@login_required
+def dashboard(request):
+    
+    doctor = doctor_models.Doctor.objects.get(user=request.user)
+    appointments = base_models.Appointment.objects.filter(doctor=doctor)
+    notifications = doctor_models.Notification.objects.filter(doctor=doctor)
+
+    context = {
+
+        "appointments": appointments,
+        "notifications" : notifications,
+
+    }
+    return render( request, "doctor/dashboard.html", context)
+
+
+@login_required
+def appointments(request):
+    
+    doctor = doctor_models.Doctor.objects.get(user=request.user)
+    appointments = base_models.Appointment.objects.filter(doctor=doctor)
+    
+
+    context = {
+
+        "appointments": appointments,
+       
+    }
+    return render( request, "doctor/appointments.html", context)
+
+
+@login_required
+def appointment_detail(request,appointment_id):
+    doctor = doctor_models.Doctor.objects.get(user=request.user)
+    appointment = base_models.Appointment.objects.get(appointment_id=appointment_id, doctor=doctor)
+    
+    medical_records= base_models.MedicalRecord.objects.filter(appointment=appointment)
+
+    lab_tests = base_models.MedicalRecord.objects.filter(appointment=appointment)
+
+    prescriptions = base_models.MedicalRecord.objects.filter(appointment=appointment)
+
+    context = {
+        "appointment":appointment,
+        "medical_records":medical_records,             
+        "lab_tests":lab_tests,
+        "prescriptions":prescriptions
+
+    }
+    return render (request, "doctor/appointment_detail.html", context)
