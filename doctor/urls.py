af612a7faf45fb31953306f33a85311511d4d2c8
Ali <ali.ozgur@example.com>
2025-01-27 23:17:50 +0300

    added dasboard page

--- doctor/urls.py
+++ doctor/urls.py
@@ -17,10 +17,27 @@ urlpatterns = [
     
     path("complete_appointment/<appointment_id>/", views.complete_appointment, name="complete_appointment"),
 
-    #medika rapor url konfigrasyonu
-
+    #medikal rapor url konfigrasyonu
     path("add_medical_report/<appointment_id>/", views.add_medical_report, name ="add_medical_report"),
 
     path("edit_medical_report/<appointment_id>/<medical_report_id>/", views.edit_medical_report, name ="edit_medical_report"),
+
+    #laboratuvar rapor url konfigrasyonu
+    path("add_lab_test/<appointment_id>/", views.add_lab_test, name="add_lab_test"),
+    path("edit_lab_test/<appointment_id>/<lab_test_id>/", views.edit_lab_test, name="edit_lab_test"),
+
+    #reçete url kongfirasyonu
+    path("add_prescription/<appointment_id>/", views.add_prescription, name="add_prescription"),
+    path("edit_prescription/<appointment_id>/<prescription_id>/", views.edit_prescription, name="edit_prescription"),
+
+    #ödeme bilgisi url konfigrasyonu
+    path("payments/", views.payments, name="payments"),
+
+    #doktor bildirimleri
+    path("notifications/", views.notifications, name="notifications"),
+    path("mark_noti_seen/<id>/", views.mark_noti_seen, name="mark_noti_seen"),
+
+    #doktor profili düzenleme
+    path("profile/", views.profile, name="profile"),
 
 ]
--- doctor/views.py
+++ doctor/views.py
@@ -46,9 +46,9 @@ def appointment_detail(request,appointment_id):
     
     medical_records= base_models.MedicalRecord.objects.filter(appointment=appointment)
 
-    lab_tests = base_models.MedicalRecord.objects.filter(appointment=appointment)
+    lab_tests = base_models.LabTest.objects.filter(appointment=appointment)
 
-    prescriptions = base_models.MedicalRecord.objects.filter(appointment=appointment)
+    prescriptions = base_models.Prescription.objects.filter(appointment=appointment)
 
     context = {
         "appointment":appointment,
@@ -98,7 +98,7 @@ def complete_appointment(request, appointment_id):
     return redirect("doctor:appointment_detail", appointment.appointment_id)
 
 
-# ekleme
+#  medical rapor ekleme
 @login_required
 def add_medical_report(request, appointment_id):
     doctor = doctor_models.Doctor.objects.get(user=request.user)
@@ -131,3 +131,151 @@ def edit_medical_report(request, appointment_id,medical_report_id):
 
     messages.success(request, "Medical Report Changed Successfully")
     return redirect("doctor:appointment_detail", appointment.appointment_id)
+
+
+# laboratuvar sonucu ekleme
+
+@login_required
+def add_lab_test(request, appointment_id):
+    doctor = doctor_models.Doctor.objects.get(user=request.user)
+    appointment = base_models.Appointment.objects.get(appointment_id=appointment_id, doctor=doctor)
+
+    if request.method == "POST":
+        test_name = request.POST.get("test_name")
+        description = request.POST.get("description")
+        result = request.POST.get("result")
+
+        base_models.LabTest.objects.create(appointment=appointment, test_name=test_name, description=description, result=result)
+
+    messages.success(request, "Lab report Added Successfully")
+    return redirect("doctor:appointment_detail", appointment.appointment_id)
+
+
+
+@login_required
+def edit_lab_test(request, appointment_id, lab_test_id):
+    doctor = doctor_models.Doctor.objects.get(user=request.user)
+    appointment = base_models.Appointment.objects.get(appointment_id=appointment_id, doctor=doctor)
+    lab_test = base_models.LabTest.objects.get(id=lab_test_id, appointment=appointment)
+
+    if request.method == "POST":
+        test_name = request.POST.get("test_name")
+        description = request.POST.get("description")
+        result = request.POST.get("result")
+
+        lab_test.test_name = test_name
+        lab_test.description = description
+        lab_test.result = result
+        lab_test.save()
+
+    messages.success(request, "Lab Report Updated Successfully")
+    return redirect("doctor:appointment_detail", appointment.appointment_id)
+
+
+#reçete ekleme
+@login_required
+def add_prescription(request, appointment_id):
+    doctor = doctor_models.Doctor.objects.get(user=request.user)
+    appointment = base_models.Appointment.objects.get(appointment_id=appointment_id, doctor=doctor)
+
+    if request.method == "POST":
+        medications = request.POST.get("medications")
+        base_models.Prescription.objects.create(medications=medications, appointment=appointment)
+
+    messages.success(request, "Prescription Added Successfully")
+    return redirect("doctor:appointment_detail", appointment.appointment_id)
+
+#reçete düzenleme
+@login_required
+def edit_prescription(request, appointment_id, prescription_id):
+    doctor = doctor_models.Doctor.objects.get(user=request.user)
+    appointment = base_models.Appointment.objects.get(appointment_id=appointment_id, doctor=doctor)
+    prescription = base_models.Prescription.objects.get(id=prescription_id)
+
+    if request.method == "POST":
+        medications = request.POST.get("medications")
+        prescription.medications = medications
+        prescription.save()
+
+    messages.success(request, "Prescription Updated Successfully")
+    return redirect("doctor:appointment_detail", appointment.appointment_id)
+
+#hastanın ödeme bilgileri
+@login_required
+def payments(request):
+    doctor = doctor_models.Doctor.objects.get(user=request.user)
+    payments = base_models.Billing.objects.filter(appointment__doctor=doctor, status="Paid")
+
+    context = {
+        "payments": payments,
+    }
+
+    return render(request, "doctor/payments.html", context)
+
+
+
+#doktor bildirimleri
+@login_required
+def notifications(request):
+    doctor = doctor_models.Doctor.objects.get(user=request.user)
+    notifications = doctor_models.Notification.objects.filter(doctor=doctor, seen=False)
+
+    context = {
+        "notifications": notifications
+    }
+
+    return render(request, "doctor/notifications.html", context)
+
+
+
+#bildirimi görüldü yapıyoruz. Ve sayfadan gidiyor.
+@login_required
+def mark_noti_seen(request, id):
+    doctor = doctor_models.Doctor.objects.get(user=request.user)
+    notification = doctor_models.Notification.objects.get(doctor=doctor, id=id)
+    notification.seen = True
+    notification.save()
+    
+    messages.success(request, "Notification marked as seen")
+    return redirect("doctor:notifications")
+
+
+#doktor profilini düzenleme
+@login_required
+def profile(request):
+    doctor = doctor_models.Doctor.objects.get(user=request.user)
+    formatted_next_available_appointment_date = doctor.next_available_appointment_date.strftime('%Y-%m-%d')
+    
+    if request.method == "POST":
+        full_name = request.POST.get("full_name")
+        image = request.FILES.get("image")
+        mobile = request.POST.get("mobile")
+        country = request.POST.get("country")
+        bio = request.POST.get("bio")
+        specialization = request.POST.get("specialization")
+        qualifications = request.POST.get("qualifications")
+        years_of_experience = request.POST.get("years_of_experience")
+        next_available_appointment_date = request.POST.get("next_available_appointment_date")
+
+        doctor.full_name = full_name
+        doctor.mobile = mobile
+        doctor.country = country
+        doctor.bio = bio
+        doctor.specialization = specialization
+        doctor.qualifications = qualifications
+        doctor.years_of_experience = years_of_experience
+        doctor.next_available_appointment_date = next_available_appointment_date
+
+        if image != None:
+            doctor.image = image
+
+        doctor.save()
+        messages.success(request, "Profile updated successfully")
+        return redirect("doctor:profile")
+
+    context = {
+        "doctor": doctor,
+        "formatted_next_available_appointment_date": formatted_next_available_appointment_date,
+    }
+
+    return render(request, "doctor/profile.html", context)
